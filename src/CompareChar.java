96f9c4f316a3137eb1b667c3fd70e548d2daa0fd
gaoweipo <gaoweipo@hotmail.com>
2023-02-20 15:07:08 -0500

    Créez un programme et modifiez-le pour l'étape suivante.

---
+++ src/CompareChar.java
@@ -0,0 +1,8 @@
+
+public class CompareChar {
+    public static void main(String[] args) {
+        char c = 'g';
+        int i = 103;
+        System.out.println("Les deux sont parrail？" + (c == i));
+    }
+}
